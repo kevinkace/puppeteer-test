f30ab7afe502c42ed11932d76c3a04cf98d8d7ac
kevinkace <kevinacameron@hotmail.com>
2017-11-17 14:35:19 -0800

    Add Path of Fire test

--- test/pof.spec.js
+++ test/pof.spec.js
@@ -24,13 +24,13 @@ afterAll(() => {
     browser.close();
 });
 
-describe("Newsletter", async () => {
-    test("can load newsletter signup form", async () => {
-        await page.goto(`${config.urlBase}/newsletter-signup/`);
+describe("Path of Fire", async () => {
+    test("can load microsite", async () => {
+        await page.goto(`${config.urlBase}/path-of-fire/`);
     });
 
-    test("can signup successfully", async () => {
-        await page.goto(`${config.urlBase}/newsletter-signup/`);
+    test("can signup to newsletters successfully", async () => {
+        await page.goto(`${config.urlBase}/path-of-fire/`);
 
         await page.click(`form [name="email"]`);
         await page.keyboard.type(user.email);
@@ -46,6 +46,6 @@ describe("Newsletter", async () => {
 
         await page.click(`form [type="submit"]`);
 
-        await page.waitForSelector("#nls-step-added", { visible : true });
+        await page.waitForSelector("#newsletter .mc869e1a60_h");
     });
 });
